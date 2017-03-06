122d915adfa2631fc37784afb202951a61bfa133
Ricardo Vasquez <rvasquezpl@gmail.com>
2017-03-06 13:45:12 -0500

    creacion de la clase Loader para cargar los archivos

---
+++ vendor/System/Controller.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vasq
+ * Date: 6/03/2017
+ * Time: 11:33
+ */
+
+namespace System;
+
+
+class Controller
+{
+
+}
---
+++ vendor/System/Loader.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vasq
+ * Date: 6/03/2017
+ * Time: 10:56
+ */
+
+namespace System;
+
+
+class Loader
+{
+
+}
