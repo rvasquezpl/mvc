35840f1412f537d14b9166db503271ff204a85e3
Ricardo Vasquez <rvasquezpl@gmail.com>
2017-03-06 18:35:56 -0500

    clases view agregadas

---
+++ vendor/System/View/ViewFactory.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vasq
+ * Date: 6/03/2017
+ * Time: 17:24
+ */
+
+namespace System\View;
+
+
+class ViewFactory
+{
+
+}
---
+++ vendor/System/View/ViewInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vasq
+ * Date: 6/03/2017
+ * Time: 17:33
+ */
+
+namespace System\View;
+
+
+interface ViewInterface
+{
+
+}
